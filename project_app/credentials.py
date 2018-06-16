456d4f78a7cbd7802c290a2ef6f3c493166ad824
dashapetrova <petrova.s.daria@gmail.com>
2018-06-16 23:54:26 +0300

    Delete credentials.py

--- project_app/credentials.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'ti1OAeuqtNYFiiBxClFxlGVbV'
-consumer_secret = 'y4DnQBwmt5acY8Ka8EksQAcKvGcY5Bo6PwWHKtHaotf8gUdTgT'
-access_token = '865277968616181760-792B86JiiE3axVao9S6XGPHdNf22UF9'
-access_token_secret = 'kQQAJoCVu80FC9nWUnV0q7BKBKlzJ9K6KN8gLGoM7ScRO'
